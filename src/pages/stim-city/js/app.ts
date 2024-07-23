159f419b7517e647d32219b829268c47d117bef8
Joshua Richardson <jrichardson@wearebond.com>
2024-07-22 19:26:32 -0600

    fix(page): override normal right-click behavior

--- src/pages/stim-city/js/app.ts
+++ src/pages/stim-city/js/app.ts
@@ -2,5 +2,9 @@ import { createScene } from './scene'
 
 window.onload = () => {
   window.scene = createScene()
+  window.addEventListener('mousedown', window.scene.onMouseDown)
+  window.addEventListener('mousemove', window.scene.onMouseMove)
+  window.addEventListener('mouseup', window.scene.onMouseUp)
+  window.addEventListener('contextmenu', event => event.preventDefault())
   window.scene.start()
 }
